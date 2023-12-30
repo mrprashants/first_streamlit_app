e9bc26bd2264535eb443c7898dd9db5cfd4cbac0
mrprashants <90003539+mrprashants@users.noreply.github.com>
2023-12-30 12:02:12 -0500

    Update streamlit_app.py
    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,12 +24,10 @@ streamlit.dataframe(fruits_to_show)
 
 # Create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-  # normalize the json data and store in a variable
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  # display the json data as a table
   return fruityvice_normalized
-streamlit.stop()
+
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -42,7 +40,7 @@ try:
     streamlit.error()
 
 # don't run anything past here while we troubleshoot
-#streamlit.stop()
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
