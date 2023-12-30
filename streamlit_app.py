d4c8d8ed4874e2e1934cb6846032c147dc614d7a
mrprashants <90003539+mrprashants@users.noreply.github.com>
2023-12-30 11:43:26 -0500

    Update streamlit_app.py
    Added a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+# Create the repeatable code block (called a function)
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+  # normalize the json data and store in a variable
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # display the json data as a table
+  return fruityvice_normalized
+
+# New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -29,12 +38,8 @@ try:
     streamlit.error("Please select a fruit to get information.")
   else:
     #streamlit.write('The user entered ', fruit_choice)
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-    # normalize the json data and store in a variable
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # display the json data as a table
-    streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
   streamlit.error()
 
 # don't run anything past here while we troubleshoot
