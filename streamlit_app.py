ff0757408749c217835a1f32cb176510939c651a
mrprashants <90003539+mrprashants@users.noreply.github.com>
2023-12-30 11:56:39 -0500

    Update streamlit_app.py
    change

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   # display the json data as a table
   return fruityvice_normalized
-
+streamlit.stop()
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -42,7 +42,7 @@ try:
     streamlit.error()
 
 # don't run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
